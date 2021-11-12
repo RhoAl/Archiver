60723184d92038333a2ecac22d3eb75a94a74adc
RhoAl <91506537+RhoAl@users.noreply.github.com>
2021-11-12 12:43:15 -0800

    third commit

---
+++ LoadSaveService.py
@@ -0,0 +1,95 @@
+import json
+
+#To anyone reviewing this, please tell me where comments would be helpful
+
+#I use the same name for the local variables of methods and the global variables declared after methods, if they have the same function.
+#I'm not sure if this keeps consistency, or if it's just confusing.
+
+toSave = {}
+
+def importSave():
+    """Imports a save from another program"""
+    filename = input("Please enter the name of the file you want to read: ")
+    try:
+        with open(filename) as file:
+            toSave = json.load(file)
+            print("File successfully opened\n")
+            return True
+    except FileNotFoundError:
+        print("No file has been loaded\n")
+        return False
+
+
+def saveDict():
+    """Adds data to the loaded-in dictionary"""
+    category = input("Enter the category for your item: ")
+    item = input("Enter the name of the item: ")
+    toSave[category] = item
+
+
+def deleteDict():
+    """Deletes data from the dictionary"""
+    #This currently works by removing the first category that matches the user inputted key
+    #If I have time, I'll try to implement a system where the user can delete on a case by case basis
+    catRemove = input("Enter the category you want to remove: ")
+    removeKey = toSave.pop(catRemove, None)
+    if (removeKey == None):
+        print("Item could not be found\n")
+    else:
+        print(catRemove, "has been successfully removed\n")
+
+
+def askDict():
+    """Asks if the user would like to edit their save file"""
+    checkLoop = True
+    while (checkLoop == True):
+        print("\nWould you like to add to a save file?\n")
+        ans = input("Enter \"Yes\" or \"No\": ")
+        firstChar = ans[0].upper()
+        if (firstChar == 'Y'):
+            saveDict()
+        else:
+            checkLoop = False
+
+    checkLoop = True
+    while (checkLoop == True):
+        print("Would you like to remove from the save file?\n")
+        ans = input("Enter \"Yes\" or \"No\": ")
+        firstChar = ans[0].upper()
+        if (firstChar == 'Y'):
+            deleteDict()
+        else:
+            checkLoop = False
+
+
+def exportSave():
+    """Exports the saved file"""
+    filename = input("Please enter the name of the file you want to write to: ") 
+    with open(filename, 'w') as file:
+        json.dump(toSave, file)
+        print("File successfully saved\n")
+
+
+#Main Program
+
+#I'm not sure if it's good or bad coding practice to initialize variables in Python; It might make it more readible, even if it's pointless in terms of making the program run.
+checkLoop = True    #Used to determine if a loop should keep looping
+checkFile = False   #Used to determine if a file has been properly loaded
+ans = ""    #Used for all answers the user wants to input
+firstChar = ""  #Used to contain the first character of the ans string
+
+while (checkLoop == True):
+    print("\nWould you like to edit a save file?\n")
+    ans = input("Enter \"Yes\" or \"No\": ")
+    firstChar = ans[0].upper()
+    
+    if (firstChar == 'N'):
+        checkLoop = False
+    elif (firstChar == 'Y'):
+        checkFile = importSave()
+        if (checkFile == True):
+            askDict()
+            exportSave()
+            checkLoop = True    #Not necessary, but it seems prudent to set it to true; in more complicated programs, this would likely just increase runtime
+    else:
+        print("Please enter only yes or no answers\n")
